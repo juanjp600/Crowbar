52be71a9bc148a80cf3462f91cdb47c0e0ed235a
Juan Pablo Arce <juanjprot@gmail.com>
2023-08-30 11:50:53 -0300

    Fixed JavaScriptSerializer not returning the types Updater expected

--- Libraries/TodoRemove/JavaScriptSerializer.cs
+++ Libraries/TodoRemove/JavaScriptSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Text;
 using System.Text.Json;
 
 namespace BackwardsCompatibility;
@@ -14,8 +15,36 @@ public class JavaScriptSerializer
         return Encoding.UTF8.GetString(stream.ToArray());
     }
 
+    private static object? ConvertJsonElementToObject(JsonElement jsonElement)
+    {
+        return jsonElement.ValueKind switch
+        {
+            JsonValueKind.Undefined => jsonElement.GetString(),
+            JsonValueKind.Object => jsonElement.EnumerateObject()
+                .Select(prop => (Key: prop.Name, Value: ConvertJsonElementToObject(prop.Value)))
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+            JsonValueKind.Array => new ArrayList(jsonElement.EnumerateArray().Select(ConvertJsonElementToObject).ToArray()),
+            JsonValueKind.String => jsonElement.GetString(),
+            JsonValueKind.Number => jsonElement.GetDouble(),
+            JsonValueKind.True => true,
+            JsonValueKind.False => false,
+            JsonValueKind.Null => null,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+    
     public T? Deserialize<T>(string str)
     {
-        return JsonSerializer.Deserialize<T>(str);
+        var result = JsonSerializer.Deserialize<T>(str);
+        if (result is Dictionary<string, object?> dictionary)
+        {
+            foreach (var kvp in dictionary.ToArray())
+            {
+                if (dictionary[kvp.Key] is not JsonElement jsonElement) { continue; }
+
+                dictionary[kvp.Key] = ConvertJsonElementToObject(jsonElement);
+            }
+        }
+        return result;
     }
 }
