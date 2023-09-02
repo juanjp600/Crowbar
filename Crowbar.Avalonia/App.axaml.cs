63e51da9c6a236411c9dfd049757011f9fd1c848
Juan Pablo Arce <juanjprot@gmail.com>
2023-09-01 22:23:23 -0300

    Put together Set Up Games tab UI, missing actual functionality

--- Crowbar.Avalonia/App.axaml.cs
+++ Crowbar.Avalonia/App.axaml.cs
@@ -1,14 +1,17 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Crowbar.Avalonia;
 
-public partial class App : Application
+public sealed partial class App : Application
 {
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        RequestedThemeVariant = ThemeVariant.Dark;
     }
 
     public override void OnFrameworkInitializationCompleted()
