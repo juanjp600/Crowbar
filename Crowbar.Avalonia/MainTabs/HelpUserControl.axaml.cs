d7576b75d502254d9b168ea97a1bb6cd9b0a3f49
Juan Pablo Arce <juanjprot@gmail.com>
2023-09-09 19:04:54 -0300

    Fully functional Help tab (wow so much work)

--- Crowbar.Avalonia/MainTabs/HelpUserControl.axaml.cs
+++ Crowbar.Avalonia/MainTabs/HelpUserControl.axaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Windows.Input;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using BackwardsCompatibility;
 
 namespace Crowbar.Avalonia.MainTabs;
 
@@ -9,5 +13,10 @@ public sealed partial class HelpUserControl : UserControl
     public HelpUserControl()
     {
         InitializeComponent();
+    }
+
+    public void LearnToUseCrowbarButtonClicked(object source, RoutedEventArgs args)
+    {
+        WebUtil.OpenUrl("https://steamcommunity.com/sharedfiles/filedetails/?id=791755353");
     }
 }
